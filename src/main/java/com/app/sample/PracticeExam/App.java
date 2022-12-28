a39af3a66b26b88d9b7a8fda672fd21523747389
Nitya <nityaapparaju@gmail.com>
2022-12-28 21:24:59 +0530

    Demo

---
+++ src/main/java/com/app/sample/PracticeExam/App.java
@@ -0,0 +1,13 @@
+package com.app.sample.PracticeExam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
